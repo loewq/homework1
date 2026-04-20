86726b35e6fe91cec1a2b7572447122dd46d8efa
loewq <blazingclock030@gmail.com>
2026-04-20 17:28:57 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,16 +3,16 @@
 using namespace std;
 
 void foo(){
-    cout << "foo" << endl;
+    
 }
 
 void bar(){
-	cout<<"bar la bar bar bar"<<endl;
+	
 }
 
 int main(){
     cout<<"hello"<<endl;
 	
-	bar()
+	
 	return 0;
 }
